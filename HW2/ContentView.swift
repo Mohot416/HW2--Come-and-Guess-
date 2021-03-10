5fcb9ec5da72b49e5861402b6950dbfef0a30122
lin1999 <menghan1117@gmail.com>
2021-03-10 13:56:05 +0800

    First Commit v1.0

--- HW2/ContentView.swift
+++ HW2/ContentView.swift
@@ -7,10 +7,78 @@
 
 import SwiftUI
 
+
+
+struct FinalPage: View {
+    var body: some View {
+        VStack{
+            Text("Hope you enjoyed in my first iOS App!")
+            Text("Play Again?")
+            NavigationLink(
+                destination: ContentView(),
+                label: {
+                    Text("Alright.").padding().foregroundColor(.blue)
+                })
+        }
+    }
+    
+}
+
+
+
+struct Game: View {
+    @State var count = 1
+    //count the number of question that player have done
+    @State var number = Int.random(in: 0...14)
+    //determine which question to be showed
+    var body: some View {
+        //let page can be roll from top to buttom
+        ScrollView(/*@START_MENU_TOKEN@*/.vertical/*@END_MENU_TOKEN@*/, showsIndicators: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/, content: {
+                VStack{
+                    //10 questions
+                    if(self.count <= 10){
+                        Button(action: {
+                            self.count += 1
+                            number = Int.random(in: 0...14)
+                        }, label: {
+                            Text("Next one~").foregroundColor(.blue).padding()
+                        })
+                        Text("Question No.\(count):")
+                        Image("AS_Unity made by Mohot416").resizable().scaledToFit().padding()
+                        Text(Questions[number].question)
+                        Text("Answer is down below...")
+                        Text(" ").padding()
+                        Text(" ").padding()
+                        Text(" ").padding()
+                        HStack{
+                            Text("Answer is ")
+                            Text(Questions[number].answer).background(Color.green)
+                        }
+                        Text("Press the link on the top to move next question, love U~").background(Color.yellow)
+                    }
+                    //jump to final page after 10 questions
+                    else{
+                        FinalPage()
+                    }
+                }
+            }
+        )
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            VStack{
+                Image("AS_Unity made by Mohot416").resizable().scaledToFit().padding()
+                Text("Hello there! I'm Jack!").background(Color.yellow)
+                Text("Let's get some fun~").background(Color.yellow)
+                //link to next page
+                NavigationLink(destination: Game()) {
+                        Text("Tap here to start!").foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/).padding()
+                    }
+            }.navigationTitle("Come and Guess!")
+        }
     }
 }
 
---
+++ HW2/PoolofQuestions.swift
@@ -0,0 +1,31 @@
+//
+//  PoolofQuestions.swift
+//  HW2
+//
+//  Created by 陳元浚 on 2021/3/9.
+//
+
+import Foundation
+
+struct QStruct {
+    var question : String
+    var answer : String
+}
+
+var Questions : [QStruct] = [
+        QStruct(question: "四分五裂(猜台灣地名)", answer: "九份"),
+        QStruct(question: "星星有多重？", answer: "八公克（星巴克）"),
+        QStruct(question: "為什麼飛機飛這麼高都不會撞到星星呢？", answer: "因為星星會閃啊"),
+        QStruct(question: "小明和小華到海邊比賽說笑話，說完笑話後，他們就死掉了，為什麼？", answer: "因為海嘯(笑)了"),
+        QStruct(question: "大熊貓一生中最大的遺憾是什麼？", answer: "沒有彩色照片"),
+        QStruct(question: "什麼時候，四減一等於五？", answer: "一個四邊形，切下一個角，還有五個角"),
+        QStruct(question: "什麼動物最愛貼在牆上？", answer: "海豹(報)"),
+        QStruct(question: "牙醫靠什麼吃飯？", answer: "嘴巴"),
+        QStruct(question: "王大嬸整天說個不停，可有一個月她說的最少，那時哪個月？", answer: "二月份"),
+        QStruct(question: "作家最後一本書是什麼書？", answer: "遺書"),
+        QStruct(question: "有風不動無風動，不動無風動有風。(猜一物)", answer: "扇子"),
+        QStruct(question: "此物大而輕，肚內火燒心。(猜一物)", answer: "燈籠"),
+        QStruct(question: "姊妹一樣長，出入都成雙，酸甜苦辣味，他們總先嘗。(猜一物)", answer: "筷子"),
+        QStruct(question: "直直一條小紅河，河水從來無浪波，天熱水位就上漲，天冷必定往下落。(猜一物)", answer: "溫度計"),
+        QStruct(question: "會走沒有腿，會吃沒有嘴，過河沒有水，死了沒有鬼。(猜一物)", answer: "象棋"),
+    ]
